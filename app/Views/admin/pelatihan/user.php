fa7919e7c6e223465652cc9faca14439e676e002
Nazaludin <ludinnaza@gmail.com>
2024-01-05 14:25:52 +0700

    fixing bug upload sertif

--- app/Views/admin/pelatihan/user.php
+++ app/Views/admin/pelatihan/user.php
@@ -194,7 +194,7 @@
                                                                 </a>
                                                             </span>
                                                         <?php } else {  ?>
-                                                            <?php if ($value['status_pelatihan'] == 'accept') { ?>
+                                                            <?php if ($value['status_pelatihan'] == 'accept' || $value['status_pelatihan'] == 'passed') { ?>
                                                                 <span data-bs-toggle="modal" data-bs-target="#modal-upload-certificate" onclick="sendUserCourseID('<?= $value['id_user_course']; ?>')">
                                                                     <a class="btn btn-icon btn-outline-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Unggah Setifikat">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
